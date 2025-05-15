b3f7d89f523c4cabc00a1849457584f0eaa699f1
chenxiaojiao <chenxiaojiao@wissonrobotics.com>
2025-05-15 15:03:23 +0800

    change slimaction param position

--- include/SlimSerialRTDE/SlimAction.h
+++ include/SlimSerialRTDE/SlimAction.h
@@ -43,7 +43,7 @@ struct ArmActionProgress{
 
 class ArmAction{
 public:
-  ArmAction(std::string _name_prefix = "",bool preemptible=true)
+  ArmAction(bool preemptible=true,std::string _name_prefix = "")
   :m_preemptible(preemptible),
   m_timeout(60),
   m_command_repeat_max(20),
